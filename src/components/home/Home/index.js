fba86fb7e235697b178168cdb2adb108bb7c7881
Cristian Gomez <gomez18cristian@gmail.com>
2021-10-31 14:43:08 -0500

    first commit

---
+++ src/components/home/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+// import {Navbar, Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+
+export default function Home() {
+    
+    return(
+    <>
+        <h1>Manhattan Crush</h1>
+    </>
+    )
+}
+
